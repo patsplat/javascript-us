461e6266af57940109c7456d9a3176c28ed73a87
Andy Burke <aburke@bitflood.org>
2016-10-06 15:34:57 -0700

    use internal data for basic lookup tests

--- test/basic_lookups.js
+++ test/basic_lookups.js
@@ -2,77 +2,41 @@
 
 const test = require('tape');
 
-const LOOKUP_STRINGS = {
-    'Alabama': 'AL',
-    'Alaska': 'AK',
-    'American Samoa': 'AS',
-    'Arizona': 'AZ',
-    'Arkansas': 'AR',
-    'California': 'CA',
-    'Colorado': 'CO',
-    'Connecticut': 'CT',
-    'Delaware': 'DE',
-    'District of Columbia': 'DC',
-    'Florida': 'FL',
-    'Georgia': 'GA',
-    'Guam': 'GU',
-    'Hawaii': 'HI',
-    'Idaho': 'ID',
-    'Illinois': 'IL',
-    'Indiana': 'IN',
-    'Iowa': 'IA',
-    'Kansas': 'KS',
-    'Kentucky': 'KY',
-    'Louisiana': 'LA',
-    'Maine': 'ME',
-    'Maryland': 'MD',
-    'Massachusets': 'MA',
-    'Michigan': 'MI',
-    'Minnesota': 'MN',
-    'Mississippi': 'MS',
-    'Missouri': 'MO',
-    'Montana': 'MT',
-    'Nebraska': 'NE',
-    'Nevada': 'NV',
-    'New Hampshire': 'NH',
-    'New Jersey': 'NJ',
-    'New Mexico': 'NM',
-    'New York': 'NY',
-    'North Carolina': 'NC',
-    'North Dakota': 'ND',
-    'Northern Mariana Islands': 'MP',
-    'Ohio': 'OH',
-    'Oklahoma': 'OK',
-    'Oregon': 'OR',
-    'Pennsylvania': 'PA',
-    'Puerto Rico': 'PR',
-    'Rhode Island': 'RI',
-    'South Carolina': 'SC',
-    'South Dakota': 'SD',
-    'Tennessee': 'TN',
-    'Texas': 'TX',
-    'Utah': 'UT',
-    'Vermont': 'VT',
-    'Virgin Islands': 'VI',
-    'Virginia': 'VA',
-    'Washington': 'WA',
-    'West Virgina': 'WV',
-    'Wisconsin': 'WI',
-    'Wyoming': 'WY'
-};
-
-test('lookups', t => {
+test('name lookups', t => {
 
     const us = require('../us.js');
 
-    Object.keys(LOOKUP_STRINGS).forEach(lookup_key => {
-        const expected_result = LOOKUP_STRINGS[lookup_key];
-        const result = us.lookup(lookup_key);
-        t.equal(result && result.abbr, expected_result, `${ lookup_key } => ${ expected_result }`);
-        if ( !result || result.abbr !== expected_result ) {
-            console.log( `${ lookup_key } = metaphone: ${ us._metaphone( lookup_key ) }` );
+    us.STATES.forEach( state => {
+        const result = us.lookup( state.name );
+        t.deepEqual( result, state, `${ state.name } => ${ state.name }`);
+        if ( !result || result.abbr !== state.abbr ) {
+            console.log( `${ state.name } = metaphone: ${ us._metaphone( state.name ) }` );
         }
-    });
+    } );
 
     t.end();
-});
+} );
+
+test('abbreviation lookups', t => {
+
+    const us = require('../us.js');
+
+    us.STATES.forEach( state => {
+        const result = us.lookup( state.abbr );
+        t.deepEqual( result, state, `${ state.abbr } => ${ state.name }`);
+    } );
+
+    t.end();
+} );
+
+test('fips lookups', t => {
+
+    const us = require('../us.js');
+
+    us.STATES.forEach( state => {
+        const result = us.lookup( state.fips );
+        t.deepEqual( result, state, `${ state.fips } => ${ state.name }`);
+    } );
+
+    t.end();
+} );
